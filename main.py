c0b2ed7f9f88e2f22c70785d9b34d75bb6e9ba2c
Rishat22 <zakir0v.rishat@mail.ru>
2020-09-29 00:15:19 +0300

    add registration window

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # This Python file uses the following encoding: utf-8
 import sys
+
 from PyQt5 import QtWidgets
 from PySide2.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
+
 from form import Ui_MainWindow
+from registration_window import Ui_RegistrationWindow
 
 MAX_WIDTH = 700
 MAX_HEIGHT = 800
 
 
-def open_registration_window():
-    print('Hello')
+def open_registration_window(self):
+    registration_window = QtWidgets.QWidget(self)
+    registration_widget = Ui_RegistrationWindow()
+    registration_widget.setupUi(registration_window)
+    registration_window.show()
 
 
 def open_login_window():
